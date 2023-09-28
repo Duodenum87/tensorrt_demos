b166edd528a8072af70bfab0c9e5ce8c3383299d
Duodenum <s62kg9910@gmail.com>
2023-09-28 17:35:43 +0800

    DFS fully implement

--- DFS.c
+++ DFS.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <unistd.h>
-
-// Constants and global variables
-#define F_NOMINAL 921600000 // Assuming a nominal frequency of 921Mhz for illustration
-#define F_MIN 76800000     // Minimum frequency
-#define F_MAX 921600000     // Maximum frequency
-#define P_THRESHOLD 7000 // Power threshold in watts
-#define DELTA_T 10     // Monitoring interval in milliseconds
-#define INCREMENT_STEP 100  // Frequency increment step
-#define DECREMENT_STEP 100  // Frequency decrement step
-#define LOW_WORKLOAD_THRESHOLD 20 // Placeholder value
-#define HIGH_WORKLOAD_THRESHOLD 80 // Placeholder value
-#define MARGIN 5 // Safety margin for power
+#include "DFS.h"
 
 int frequencies[] = {76800000, 153600000, 230400000, 307200000, 384000000, 460800000, 537600000, 614400000, 691200000, 768000000, 844800000, 921600000};
 int current_frequency_index = 11;
+
+void calculate_power(float timelapse)
+{
+    int power = read_power();
+    FILE *fp = fopen("./power_consump.txt", "a");
+    if (!fp) {
+        perror("Failed to create txt");
+        return;
+    }
+
+    fprintf(fp, "%d\t%f\n", power, timelapse);
+    
+    fclose(fp);
+    return;
+}
 
 int read_power() 
 {
@@ -56,36 +56,96 @@ int read_GPU_usage()
     return usage;
 }
 
-void set_frequency(int index)
+void update_frequency_index()
 {
-    if (index >= 0 && index < sizeof(frequencies) / sizeof(frequencies[0])) {
-        current_frequency_index = index;
-        printf("Frequency set to: %dHz\n", frequencies[current_frequency_index]);
-    } 
+    FILE *fp = fopen("/sys/devices/57000000.gpu/devfreq/57000000.gpu/cur_freq", "r");
+    if (!fp) {
+        perror("Failed to open the file");
+        return;
+    }
+
+    int cur_freq;
+    if (fscanf(fp, "%d", &cur_freq) != 1) {
+        fprintf(stderr, "Failed to read frequency value\n");
+        fclose(fp);
+        return;
+    }
+    fclose(fp);
+
+    // linear search from frequency[]
+    bool found = 0;
+    for (int i = 0; i < sizeof(frequencies) / sizeof(frequencies[0]); i++) {
+        if (frequencies[i] == cur_freq) {
+            current_frequency_index = i;
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        fprintf(stderr, "finding frequency from file error\n");
+    }
+    return;
 }
 
 void increase_freq()
 {
     if (current_frequency_index < sizeof(frequencies) / sizeof(frequencies[0]) - 1) {
-        set_frequency(current_frequency_index + 1);
+        int index = current_frequency_index + 1;
+        if (index >= 0 && index < sizeof(frequencies) / sizeof(frequencies[0])) {
+            FILE *file1, *file2;
+            file1 = fopen("/sys/devices/57000000.gpu/devfreq/57000000.gpu/max_freq", "w");
+            file2 = fopen("/sys/devices/57000000.gpu/devfreq/57000000.gpu/min_freq", "w");
+
+            if (file1 == NULL || file2 == NULL) {
+                perror("Error opening file");
+                return;
+            }
+
+            fprintf(file1, "%d", frequencies[index]);
+            fprintf(file2, "%d", frequencies[index]);
+            printf("Frequency set to: %dHz\n", frequencies[index]);
+
+            fclose(file1);
+            fclose(file2);
+        }
     }
 }
 
 void decrease_freq()
 {
     if (current_frequency_index > 0) {
-        set_frequency(current_frequency_index - 1);
+        int index = current_frequency_index - 1;
+        if (index >= 0 && index < sizeof(frequencies) / sizeof(frequencies[0])) {
+            FILE *file1, *file2;
+            file1 = fopen("/sys/devices/57000000.gpu/devfreq/57000000.gpu/min_freq", "w");
+            file2 = fopen("/sys/devices/57000000.gpu/devfreq/57000000.gpu/max_freq", "w");
+
+            if (file1 == NULL || file2 == NULL) {
+                perror("Error opening file");
+                return;
+            }
+
+            fprintf(file1, "%d", frequencies[index]);
+            fprintf(file2, "%d", frequencies[index]);
+            printf("Frequency set to: %dHz\n", frequencies[index]);
+
+            fclose(file1);
+            fclose(file2);
+        }
     }
 }
 
 int main()
 {
-    while (1)
-    {
-        sleep(DELTA_T / 1000);
+    // while (1)
+    // {
+    //     sleep(DELTA_T / 1000);
 
         int power = read_power();
         int workload = read_GPU_usage();
+
+        update_frequency_index();
 
         if (power > P_THRESHOLD) {
             decrease_freq();
@@ -95,6 +155,6 @@ int main()
             increase_freq();
         }
 
-    }
+    // }
     return 0;
 }
---
+++ DFS.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+// Constants and global variables
+#define F_NOMINAL 921600000 // Assuming a nominal frequency of 921Mhz for illustration
+#define F_MIN 76800000     // Minimum frequency
+#define F_MAX 921600000     // Maximum frequency
+#define P_THRESHOLD 7000 // Power threshold in watts
+#define DELTA_T 10     // Monitoring interval in milliseconds
+#define INCREMENT_STEP 100  // Frequency increment step
+#define DECREMENT_STEP 100  // Frequency decrement step
+#define LOW_WORKLOAD_THRESHOLD 20 // Placeholder value
+#define HIGH_WORKLOAD_THRESHOLD 80 // Placeholder value
+#define MARGIN 5 // Safety margin for power
+
+void calculate_power(float);
+int read_power();
